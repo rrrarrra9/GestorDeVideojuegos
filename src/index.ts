6aac1a5c18242bcd581a097d3c26a3382096247a
raul <rrrarrra100@gmail.com>
2026-02-28 01:17:01 +0100

    chore(server): setup Express server with dotenv config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('🎮 API de Videojuegos - Servidor iniciado');
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express()
+const port = process.env.PORT ?? 3000
+
+
+
+app.get("/", (req, res) => {
+    res.json({ status: "ok", message: "api de videojuegos" })
+})
+
+
+app.listen(port, () => {
+    console.log(`servidor corriendo en puerto ${port}`);
+})
